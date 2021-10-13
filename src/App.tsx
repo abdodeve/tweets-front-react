8f2179cfca9d174bbbf4cd81f6b51785a0af7df7
Abdelhadi HABCHI <ahabchi@smileandpay.com>
2021-10-13 13:35:34 +0200

    Prepare the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { io } from "socket.io-client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import theme from "./Theme";
+import GlobalStyles from "./Theme/GlobalStyles";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const ENDPOINT: string = "http://localhost:8081";
+const socket = io(ENDPOINT);
 
 function App() {
+  console.log("abc");
+  // client-side
+
+  // socket.on("disconnect", () => {
+  //   console.log("disconnect==>", socket.id); // undefined
+  // });
+
+  const [response, setResponse] = useState<string[]>(["abc", "efg"]);
+  const [myName, setMyName] = useState<string>("Abde");
+  const [loadClient, setLoadClient] = useState(true);
+
+  useEffect(() => {
+    console.log("Use effet works");
+    socket.on("connect", () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+
+    socket.on("news_by_server", (data) => {
+      console.log(data);
+      setResponse((prev) => {
+        return addTweet(prev, data);
+      });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [loadClient]);
+
+  function addTweet(list: string[], newEl: string) {
+    const newList = [newEl, ...list];
+    newList.length = 5;
+    return newList;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => setLoadClient((prevState) => !prevState)}>
+            STOP CLIENT
+          </button>
+          <button
+            onClick={() => {
+              console.log("works");
+              setMyName(Math.random().toString());
+              //response.push("Abdel");
+              setResponse((resp) => [...response, "Abdooo1"]);
+            }}
+          >
+            Check
+          </button>
+          <h1>{myName}</h1>
+          {loadClient ? (
+            <>
+              {response.map((value, index) => {
+                return (
+                  <p style={{ fontSize: 10 }} key={index}>
+                    {value}
+                  </p>
+                );
+              })}
+            </>
+          ) : null}
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Theme/index.tsx
@@ -0,0 +1,26 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import colors from "@mui/material/colors";
+
+import typography from "./typography";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: colors.common.white,
+      paper: colors.common.white,
+    },
+    primary: {
+      main: colors.indigo[500],
+    },
+    secondary: {
+      main: colors.indigo[500],
+    },
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600],
+    },
+  },
+  typography,
+});
+
+export default theme;
